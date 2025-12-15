18d42f1d66b93535e6428400074d2eac75f2f04a
Nattha <gamenattha555@gmail.com>
2025-12-15 17:17:03 +0700

    fix image

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "../components/Navbar";
 
 export function Layout() {
   return (
-    <div className="bg-[url('../public/Forest.jpg')] bg-no-repeat bg-cover">
+    <div className="bg-[url('../public/images/Forest.jpg')] bg-no-repeat bg-cover">
       <Navbar />
       <section className= "flex justify-center">
         <Outlet />
