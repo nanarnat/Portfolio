9b0c4171d37576c672c88e27907bd5ea60bc71e8
Nattha <gamenattha555@gmail.com>
2025-12-15 17:14:12 +0700

    add picture

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,13 +4,13 @@ import axios from "axios";
 export default function Home() {
  
   return (
-    <div className="min-h-screen p-6 gap-y-6 flex flex-col justify-start w-full bg-[url('../public/animal_stand_neko.png')] bg-no-repeat bg-center ">
+    <div className="min-h-screen p-6 gap-y-6 flex flex-col justify-start w-full  ">
       <section className="mt-10 relative ">
         <h1 className="text-5xl font-bold text-center text-black absolute inset-0 translate-x-[4px] translate-y-[4px] ">The pet game</h1>
         <h1 className="text-5xl font-bold text-center text-white relative">The pet game</h1>
       </section>
-      <section className="w-full flex justify-center mt-55">
-        <button className="border bg-indigo-500 text-white text-2xl py-1 px-3 rounded-3xl shadow-lg hover:scale-125">
+      <section className="w-full h-full flex justify-center items-center">
+        <button className="border bg-indigo-500 text-white text-2xl pb-2 px-4 rounded-3xl shadow-lg hover:scale-125">
           <p>play</p>
         </button>
       </section>
